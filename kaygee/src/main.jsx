7989ccb8341f7a400f6d9747eefeebbd8f80bee0
Linus Godwin <linusgodwin2@gmail.com>
2025-12-07 15:14:55 +0100

    error

--- kaygee/src/main.jsx
+++ kaygee/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./styles/app.css";
+import "./styles/App.css";
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from "./ThemeContext";
 
